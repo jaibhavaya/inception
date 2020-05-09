a51a269e28019beb3a7f7c1795ad4eba77be50dd
Jacob Lawson <jaibhavaya@gmail.com>
2020-05-08 23:23:43 -0400

    playing around with command line args

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var supportedCommands = [4]string{
+	"list-projects",
+	"read-project",
+	"render-pipeline",
+	"self-test",
+}
 
 func main() {
-  log.Print("hello world")
+	if len(os.Args) != 2 {
+		fmt.Println("you dun f!@#ed up")
+		usage()
+		os.Exit(1)
+	}
+	command := os.Args[1]
+	if !isSupportedCommand(command) {
+		fmt.Println(command, "is an unsupported command, please use one of the following:", strings.Join(supportedCommands[:], ", "))
+		os.Exit(2)
+	}
+	fmt.Println("this command is goooood: ", command)
+}
+
+func isSupportedCommand(command string) bool {
+	found := false
+	for _, c := range supportedCommands {
+		if c == command {
+			found = true
+			break
+		}
+	}
+	return found
+}
+
+func usage() {
+	fmt.Println("usage: \nscript command")
 }
